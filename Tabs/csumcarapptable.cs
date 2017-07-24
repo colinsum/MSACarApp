b41d52fae6c0dff87d1afc40716af6d5b8b36ca5
colinsum <colinsum8@gmail.com>
2017-07-24 14:47:18 +1200

    Got backend connected

---
+++ Tabs/csumcarapptable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.MobileServices;
+
+
+namespace Tabs
+{
+    public class csumcarapptable
+
+
+
+    {
+
+        [JsonProperty(PropertyName = "Id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "make")]
+        public string make { get; set; }
+
+        [JsonProperty(PropertyName = "model")]
+        public string model { get; set; }
+
+        [JsonProperty(PropertyName = "engine")]
+        public string engine { get; set; }
+
+    }
+}
